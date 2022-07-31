8952c81fca98084f58a79a970932129a76ccaf67
chromodyne <dsinitiere@outlook.com>
2022-07-31 16:59:08 -0700

    All tag-routes now functioning properly.

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -7,7 +7,13 @@ router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
   try {
+    const tag = await Tag.findAll({
+      include : {
+        model: Product
+      }
+    });
 
+    res.status(200).json(tag);
   } catch (err) {
     res.status(500).json(err);
   }
@@ -17,7 +23,18 @@ router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
   try {
+    const tag = await Tag.findByPk(req.params.id, {
+      include: {
+        model: Product
+      }
+    })
 
+    if(!tag) {
+      res.status(404).json({ message: "That tag does not exist." });
+      return;
+    }
+
+    res.status(200).json(tag);
   } catch (err) {
     res.status(500).json(err);
   }
@@ -26,6 +43,12 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
   // create a new tag
   try {
+
+    const tag = await Tag.create({
+      tag_name: req.body.tag_name
+    });
+
+    res.status(200).json(tag);
 
   } catch (err) {
     res.status(500).json(err);
@@ -36,6 +59,19 @@ router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
   try {
 
+    const tag = await Tag.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    })
+
+    if(!tag[0]) {
+      res.status(400).json({ message: "That tag does not exist." });
+      return;
+    }
+
+    res.status(200).json(tag);
+
   } catch (err) {
     res.status(500).json(err);
   }
@@ -44,6 +80,19 @@ router.put('/:id', async (req, res) => {
 router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
   try {
+
+    const tag = Tag.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!tag) {
+      res.status(404).json({ messsage: "That tag does not exist." });
+      return;
+    }
+
+    res.status(200).json(tag);
 
   } catch (err) {
     res.status(500).json(err);
